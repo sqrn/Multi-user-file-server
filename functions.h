7dc71c319b74af7804b598a4aceb648f17ab512d
Mariusz Skóra <mariusz@mariusLapk.(none)>
2011-06-27 23:06:28 +0200

    find_file, get_file

--- functions.h
+++ functions.h
@@ -10,7 +10,7 @@ Plik nagłówkowy
 #include <sys/socket.h>
 #include <netinet/in.h>
 int send_message(int clientFd, struct sockaddr_in clientaddr, char textbuffer[MAX_BUFFER+1]);
-int send_file(int clientFd, struct sockaddr_in clientaddr, char filename[32]);
+int send_file(int clientFd, struct sockaddr_in clientaddr, char filename[32], int cli_id);
 int find_file(int clientFd, struct sockaddr_in clientaddr, char filename[32]);
 void save_file_list(char buff[255], char clientNo[16]);
 #endif
--- include/send_file.c
+++ include/send_file.c
@@ -2,7 +2,8 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <stdio.h>
-#define MAX_BUFFER 80
+#include "fun.h"
+
 
 /**
 Funkcja przesyla do klienta wskazany przez niego plik.
@@ -12,37 +13,43 @@ ETAPY:
 3. Jeżeli plik nie zostanie znaleziony, funkcja zwraca -1.
 4. Po poprawnym ukończeniu przesyłania danych, funkcja zwraca 0.
 */
-int send_file(int clientFd, struct sockaddr_in clientaddr, char filename[32])
+int send_file(int clientFd, struct sockaddr_in clientaddr, char filename[32], int cli_id)
 {
-    int textDl;
     char textbuffer[MAX_BUFFER+1];
-    FILE *fh;
     int whoHasFile;
+    char cli_port[20];
+    char cli_addr[20];
+
 
     printf("INFO: Klient chce otrzymac plik: %s\n",filename);
     /* Dopisanie do funkcji nazwy folderu przeszukiwania */
 
-    if( (whoHasFile = find_file(clientFd, clientaddr, filename)) < 0 )
-        return -1;
+    printf("Plik posiada CLIENT_%i", cli_id);
 
+/*    if(getpeername(clientFd, (struct sockaddr *)&clientaddr, &klientDl) != 0)
+        perror("ERROR: getpeername()");
+    else
+    {
+        printf("INFO: Doszedl nowy klient: %s:%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+        sprintf(cli_addr, "%s", inet_ntoa(clientaddr.sin_addr));
+        sprintf(cli_port, "%d", ntohs(clientaddr.sin_port));
+
+    }
+*/
     /* TODO:
         Trzeba zlapac dane adresowe klienta o ID whoHasFILE - CLIENT_whoHasFile
         Nastepnie przeslac dane adresowe tego goscia co ten plik posiada
     */
+    /* przesyla do klienta dane adresowe klienta, ktory posiada plik */
+    /*
+    przeslac krotke do klienta:
+    ['cli_addr','port']
 
-    if ((fh = fopen(filename, "r")) != NULL)
-    {
-        while (!feof(fh))
-        {
-            fread(textbuffer, 1, MAX_BUFFER, fh);
-            textDl = strlen(textbuffer);
-            if (send(clientFd, textbuffer, textDl, 0) < 0)
-                return -1;
-        }
-        fclose(fh);
-    }
-    else
+    sprintf(textbuffer, "['%s','%s']", addr, port);
+    printf("%s", textbuffer);
+    if (send(clientFd, textbuffer, sizeof(textbuffer), 0) < 0)
         return -1;
-
+*/
     return 0;
 }
